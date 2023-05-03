f386a171183daeea40db612c4bb560fc892a8eb9
facupujol <facundopujolsc@gmail.com>
2023-05-02 23:37:34 -0300

    ej6 mejorado

--- P3/Ej6/Ej6.js
+++ P3/Ej6/Ej6.js
@@ -4,24 +4,33 @@ selector.addEventListener("change", traerPaises);
 
 const url = "https://restcountries.com/v3.1/all";
 
+function mapPaises(paises){
+    console.log(paises[0]);
+    return paises.map((p) => ({
+        name: p.name.common, 
+        capital: p.capital, 
+        population: p.population,
+        region: p.region,
+        subregion: p.subregion,
+    }));
+}
+
 function traerPaises() {
     fetch(url)
     .then(rpta => rpta.json())
     .then(data => {
-        const paises = Object.values(data);
+        const paises = mapPaises(data);
         ordenarPaises(paises, selector.value);
         const list = document.getElementById("lista");
         list.innerHTML = "";
         paises.forEach(pais => {
             const elemento = document.createElement("li");
-            elemento.textContent = "Nombre: " + pais.name.common + ", Capital: " + pais.capital + ", Poblacion: " + pais.population + ", Region: " + pais.region + ", Subregion: " + pais.subregion;
+            elemento.textContent = "Nombre: " + pais.name + ", Capital: " + pais.capital + ", Poblacion: " + pais.population + ", Region: " + pais.region + ", Subregion: " + pais.subregion;
             elemento.style.fontSize = "20px";
             list.appendChild(elemento);
         });
     });
 }
-
-// No ordena bien por nombre
 
 function ordenarPaises(paises, criterio){
     paises.sort((a, b) => {
